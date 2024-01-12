f685223df71cce672a7a760b933082585ee50ac7
Britt Shook <brittanysshook@gmail.com>
2024-01-12 15:22:05 -0500

    get user data

---
+++ modules/user.js
@@ -0,0 +1,12 @@
+import { currentToken } from "./token.js";
+
+async function getUserData() {
+  const response = await fetch("https://api.spotify.com/v1/me", {
+    method: "GET",
+    headers: { Authorization: "Bearer " + currentToken.access_token },
+  });
+
+  return await response.json();
+}
+
+export const userData = await getUserData();
