388f1d76805ff03d5fb99ecc21bc1cd6bddd5fda
Britt Shook <brittanysshook@gmail.com>
2024-01-12 15:38:09 -0500

    update scope permissions

--- modules/config.js
+++ modules/config.js
@@ -4,4 +4,4 @@ export const redirectUrl = "http://localhost:5500/";
 export const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 export const tokenEndpoint = "https://accounts.spotify.com/api/token";
 export const scope =
-  "user-read-private user-read-email user-top-read user-library-read playlist-modify-private";
+  "user-read-private user-read-email user-top-read user-library-read playlist-modify-private playlist-read-private playlist-read-collaborative";
