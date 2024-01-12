7ffd002d539694fff8a6141989bc7b3b78bba191
Britt Shook <brittanysshook@gmail.com>
2024-01-12 15:21:30 -0500

    add config file

---
+++ modules/config.js
@@ -0,0 +1,7 @@
+export const clientId = "fefd94ed293544c8b74cc8b2ae78a1af";
+export const redirectUrl = "http://localhost:5500/";
+
+export const authorizationEndpoint = "https://accounts.spotify.com/authorize";
+export const tokenEndpoint = "https://accounts.spotify.com/api/token";
+export const scope =
+  "user-read-private user-read-email user-top-read user-library-read playlist-modify-private";
