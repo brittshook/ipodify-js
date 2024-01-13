aa237cd8ddfb1c1b9f60c439078f74d1c100f4b1
Britt Shook <brittanysshook@gmail.com>
2024-01-13 16:14:48 -0500

    cache data after current token validation

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import { renderTemplate } from "./modules/template.js";
 import { currentToken } from "./modules/token.js";
-import { userData } from "./modules/user.js";
+import { getUserData } from "./modules/user.js";
 import { getToken } from "./modules/auth.js";
+import { getAlbumItems, updateSlider } from "./modules/components/slider.js";
 
 const args = new URLSearchParams(window.location.search);
 const code = args.get("code");
@@ -18,7 +19,11 @@ if (code) {
 }
 
 if (currentToken.access_token) {
+  const userData = await getUserData();
+  const albumItems = await getAlbumItems();
   renderTemplate("main", "ipodify-home", userData);
+  renderTemplate("screen-content", "home-screen", albumItems);
+  updateSlider();
   renderTemplate("oauth", "oauth-template", currentToken);
 }
 
