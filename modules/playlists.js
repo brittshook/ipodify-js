2c74ca30caea3cc1335a9d6001dacbf4c8fc5af1
Britt Shook <brittanysshook@gmail.com>
2024-01-14 00:24:56 -0500

    update to fetch playlist items, display playlists

--- modules/playlists.js
+++ modules/playlists.js
@@ -1,18 +1,59 @@
-import { currentToken } from "./token.js";
+import { currentToken } from "./auth/token.js";
 import { getUserData } from "./user.js";
+import { displayList } from "./list.js";
 
 const userData = await getUserData();
-const userId = userData.id;
-console.log(userId);
-const userPlaylistsEndpoint = `https://api.spotify.com/v1/users/${userId}/playlists/`;
+const userId = () => (userData.id != "undefined" ? userData.id : undefined);
+console.log(userId());
+const userPlaylistsEndpoint = `https://api.spotify.com/v1/users/${userId()}/playlists/`;
+const playlistItemsEndpoint = (playlist) =>
+  `https://api.spotify.com/v1/playlists/${playlist.id}/tracks`;
 
 export async function getPlaylists() {
-  const response = await fetch(userPlaylistsEndpoint, {
+  const params = {
+    limit: 50,
+  };
+
+  const url = new URL(userPlaylistsEndpoint);
+  url.search = new URLSearchParams(params);
+
+  const response = await fetch(url, {
     method: "GET",
     headers: { Authorization: "Bearer " + currentToken.access_token },
   });
 
   return await response.json();
+}
+
+export async function getPlaylistItems(playlist, offset = 0) {
+  const items = [];
+  const limit = 50;
+
+  const params = {
+    limit: limit,
+    offset: offset,
+  };
+
+  const url = new URL(playlistItemsEndpoint(playlist));
+  url.search = new URLSearchParams(params);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + currentToken.access_token },
+    });
+
+    const data = await response.json();
+    const newItems = data.items;
+
+    if (newItems.length > 0) {
+      items.push(...newItems);
+      offset += limit;
+      await getPlaylistItems(playlist, offset);
+    }
+  } catch (er) {
+    console.log("Error fetching playlist items:", er);
+  }
 }
 
 export async function createPlaylist(playlistName, playlistDescription) {
@@ -33,18 +74,16 @@ export async function createPlaylist(playlistName, playlistDescription) {
 }
 
 export async function addPlaylistItems(playlist, trackArr) {
-  const playlistId = playlist.id;
   const itemURIs = trackArr
     .slice(0, 100)
     .map(
       (item) =>
         `spotify:${item.type == "track" ? "track" : "episode"}:${item.id}`
     );
-  const addPlaylistItemsEndpoint = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
 
   console.log(JSON.stringify({ uris: itemURIs }));
 
-  const response = await fetch(addPlaylistItemsEndpoint, {
+  const response = await fetch(playlistItemsEndpoint(playlist), {
     method: "POST",
     headers: {
       Authorization: "Bearer " + currentToken.access_token,
@@ -56,4 +95,19 @@ export async function addPlaylistItems(playlist, trackArr) {
   });
 
   return await response.json();
+}
+
+export async function displayPlaylists() {
+  const userPlaylistsObj = await getPlaylists();
+  const userPlaylists = userPlaylistsObj.items;
+
+  let playlists = [];
+  let eventHandlers = [];
+
+  for (const playlist of userPlaylists) {
+    playlists.push(playlist.name);
+    eventHandlers.push(() => console.log(playlist));
+  }
+
+  displayList(playlists, eventHandlers);
 }
