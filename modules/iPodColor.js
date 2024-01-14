08bb2ccaaa951276bcf16d134e50a1f524d2a436
Britt Shook <brittanysshook@gmail.com>
2024-01-14 00:23:59 -0500

    dynamically change control color based on iPod color

--- modules/iPodColor.js
+++ modules/iPodColor.js
@@ -1,7 +1,8 @@
 export const setIpodColor = (e) => {
-  const color = e.currentTarget.classList;
+  const color = e.currentTarget.className;
   const mainBody = document.querySelector("#main-body");
   const topBody = document.querySelector("#top-body");
+  const controls = document.querySelector("#controls svg");
 
   const applyColor = (el) => {
     el.classList.forEach((className) => {
@@ -9,6 +10,13 @@ export const setIpodColor = (e) => {
     });
     el.classList.add(color);
   };
+
+  console.log(color);
+  if (color === "gray") {
+    controls.style.fill = "#111";
+  } else {
+    controls.style.fill = "#fff";
+  }
 
   [mainBody, topBody].forEach(applyColor);
 };
