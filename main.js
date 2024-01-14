52a5b378cf761aeee4541561f2eba2d5c6ecea8f
Britt Shook <brittanysshook@gmail.com>
2024-01-14 00:22:46 -0500

    add logic to automatically refresh tokens

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { renderTemplate } from "./modules/template.js";
-import { currentToken } from "./modules/token.js";
+import { currentToken } from "./modules/auth/token.js";
 import { getUserData } from "./modules/user.js";
-import { getToken } from "./modules/auth.js";
-import { getAlbumItems, updateSlider } from "./modules/components/slider.js";
+import { getToken, refreshToken } from "./modules/auth/auth.js";
+import { displaySliderItems } from "./modules/slider.js";
 
 const args = new URLSearchParams(window.location.search);
 const code = args.get("code");
@@ -18,15 +18,26 @@ if (code) {
   window.history.replaceState({}, document.title, updatedUrl);
 }
 
-if (currentToken.access_token) {
-  const userData = await getUserData();
-  const albumItems = await getAlbumItems();
-  renderTemplate("main", "ipodify-home", userData);
-  renderTemplate("screen-content", "home-screen", albumItems);
-  updateSlider();
-  renderTemplate("oauth", "oauth-template", currentToken);
+if (currentToken.access_token && currentToken.isExpired()) {
+  const token = await refreshToken();
+  currentToken.save(token);
 }
 
-if (!currentToken.access_token) {
+if (currentToken.access_token) {
+  const userData = await getUserData();
+
+  renderTemplate("main", "ipodify-home", userData);
+  renderTemplate("screen-content", "home-screen");
+  displaySliderItems();
+} else if (!currentToken.access_token) {
   renderTemplate("main", "login");
 }
+
+setInterval(() => {
+  if (currentToken.access_token && currentToken.isExpired()) {
+    console.log("is expired");
+    refreshToken()
+      .then((token) => currentToken.save(token))
+      .catch((er) => console.log("Error refreshing token:", er));
+  }
+}, 60000);
