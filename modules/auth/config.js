d231f2b6119ceff6dea79f7c6e9d686ced63510b
Britt Shook <brittanysshook@gmail.com>
2024-01-15 19:17:33 -0500

    update redirect to netlify site

--- modules/auth/config.js
+++ modules/auth/config.js
@@ -1,5 +1,5 @@
 export const clientId = "fefd94ed293544c8b74cc8b2ae78a1af";
-export const redirectUrl = "http://localhost:5500";
+export const redirectUrl = "https://ipodify.netlify.app";
 
 export const authorizationEndpoint = "https://accounts.spotify.com/authorize";
 export const tokenEndpoint = "https://accounts.spotify.com/api/token";
