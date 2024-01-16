5472ac789311b3328cc12612cb30f22ea10d97ee
Britt Shook <brittanysshook@gmail.com>
2024-01-15 19:00:20 -0500

    handle bottom body element

--- modules/iPodColor.js
+++ modules/iPodColor.js
@@ -2,6 +2,7 @@ export const setIpodColor = (e) => {
   const color = e.currentTarget.className;
   const mainBody = document.querySelector("#main-body");
   const topBody = document.querySelector("#top-body");
+  const bottomBody = document.querySelector("#bottom-body");
   const controls = document.querySelector("#controls svg");
 
   const applyColor = (el) => {
@@ -11,12 +12,11 @@ export const setIpodColor = (e) => {
     el.classList.add(color);
   };
 
-  console.log(color);
   if (color === "gray") {
     controls.style.fill = "#111";
   } else {
     controls.style.fill = "#fff";
   }
 
-  [mainBody, topBody].forEach(applyColor);
+  [mainBody, topBody, bottomBody].forEach(applyColor);
 };
